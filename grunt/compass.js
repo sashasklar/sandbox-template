251dcecd8561431675260a78b1dc754737fcc1af
Sasha Sklar <sashasklar@gmail.com>
2015-02-18 05:14:38 -0800

    Moves generated CSS to local directory to work around SASS sourcemaps pathing issue

--- grunt/compass.js
+++ grunt/compass.js
@@ -1,8 +1,9 @@
 module.exports = {
   options: {
     sassDir: '<%= paths.src %>/scss',
-    cssDir: '<%= paths.tmp %>/css',
-    sourcemap: true
+    cssDir: '<%= paths.src %>/css',
+    sourcemap: true,
+    relativeAssets: false
   },
   dev: {
 
